a57aa164cfded3ef513268f156c8d907d06070e5
GLbIBA <romanovskyviktor@gmail.com>
2024-08-17 17:26:44 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+homework_count='12'
+hour_count='1.5'
+course_name='Python'
+time_one_homework=(float(hour_count))/(int(homework_count))
+print('Курс, всего задач:', homework_count, ', затрачено часов: ', hour_count, ', среднее время выполнения ', time_one_homework, ' часа')
