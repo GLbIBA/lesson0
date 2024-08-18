ae407c8b40c1496a9295887c8084f2a545dfa8e2
GLbIBA <romanovskyviktor@gmail.com>
2024-08-18 19:33:07 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name='Viktor'
+age=34
+new_age=age+int(5)
+is_students=bool('yes')
+print('Name:',name) #, end='\n')
+print('Age:',age)
+print('New age:',new_age)
+print('Is students:',is_students)
+
